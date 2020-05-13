d7bc00b13c1b44c28ebbd4b3559f21546b84a153
Felipe Andrade <felipe_fac_8@hotmail.com>
2020-05-13 10:22:55 -0300

    Inicio da parte de revisão e confirmação do pedido

--- src/app/pages/checkout/components/confirmacao/confirmacao.component.ts
+++ src/app/pages/checkout/components/confirmacao/confirmacao.component.ts
@@ -1,3 +1,7 @@
+import { OrderModel } from './../../../../models/order-model';
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { OrderStore } from './../../../../actions/order-action';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmacaoComponent implements OnInit {
 
-  constructor() { }
+  order$: Observable<OrderModel>;
+
+  constructor(private orderStore: Store<OrderStore>, ) {
+    this.order$ = this.orderStore.pipe(select('order'));
+
+  }
 
   ngOnInit(): void {
+    this.order$.subscribe(order => {
+      console.log('confirmação: ', order);
+    });
   }
 
 }
